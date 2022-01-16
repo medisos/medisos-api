494f3b1d734ea5c3de6df382be6d7a29b1757795
jeansaigne <aymerictibere@gmail.com>
2022-01-16 21:18:17 +0100

    feat: Added Home controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HomeController } from './controllers';
+import { HomeService } from './services';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [HomeController],
+  providers: [HomeService],
 })
 export class AppModule {}
